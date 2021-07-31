354c67a09b28911698e5195db6af0ad4f6cb105a
mct <minsa97@naver.com>
2021-07-31 15:16:08 +0900

    ch02

--- ch02_variable/02_gugudan.c
+++ ch02_variable/02_gugudan.c
@@ -2,38 +2,85 @@
 
 #include <stdio.h>
 int main(void){
-    printf("%d*%d=%d\n", 2,1,2*1);
-    printf("%d*%d=%d\n", 2,2,2*2);
-    printf("%d*%d=%d\n", 2,3,2*3);
-    printf("%d*%d=%d\n", 2,4,2*4);
-    printf("%d*%d=%d\n", 2,5,2*5);
-    printf("%d*%d=%d\n", 2,6,2*6);
-    printf("%d*%d=%d\n", 2,7,2*7);
-    printf("%d*%d=%d\n", 2,8,2*8);
+    printf("%d*%d=%d\t", 2,1,2*1);
+    printf("%d*%d=%d\t", 2,2,2*2);
+    printf("%d*%d=%d\t", 2,3,2*3);
+    printf("%d*%d=%d\t", 2,4,2*4);
+    printf("%d*%d=%d\t", 2,5,2*5);
+    printf("%d*%d=%d\t", 2,6,2*6);
+    printf("%d*%d=%d\t", 2,7,2*7);
+    printf("%d*%d=%d\t", 2,8,2*8);
     printf("%d*%d=%d\n", 2,9,2*9);
-    printf("---------\n");
     
-    printf("%d*%d=%d\n", 3,1,3*1);
-    printf("%d*%d=%d\n", 3,2,3*2);
-    printf("%d*%d=%d\n", 3,3,3*3);
-    printf("%d*%d=%d\n", 3,4,3*4);
-    printf("%d*%d=%d\n", 3,5,3*5);
-    printf("%d*%d=%d\n", 3,6,3*6);
-    printf("%d*%d=%d\n", 3,7,3*7);
-    printf("%d*%d=%d\n", 3,8,3*8);
+    printf("%d*%d=%d\t", 3,1,3*1);
+    printf("%d*%d=%d\t", 3,2,3*2);
+    printf("%d*%d=%d\t", 3,3,3*3);
+    printf("%d*%d=%d\t", 3,4,3*4);
+    printf("%d*%d=%d\t", 3,5,3*5);
+    printf("%d*%d=%d\t", 3,6,3*6);
+    printf("%d*%d=%d\t", 3,7,3*7);
+    printf("%d*%d=%d\t", 3,8,3*8);
     printf("%d*%d=%d\n", 3,9,3*9);
-    printf("---------\n");
     
-    printf("%d*%d=%d\n", 4,1,2*1);
-    printf("%d*%d=%d\n", 4,2,4*2);
-    printf("%d*%d=%d\n", 4,3,4*3);
-    printf("%d*%d=%d\n", 4,4,4*4);
-    printf("%d*%d=%d\n", 4,5,4*5);
-    printf("%d*%d=%d\n", 4,6,4*6);
-    printf("%d*%d=%d\n", 4,7,4*7);
-    printf("%d*%d=%d\n", 4,8,4*8);
+    printf("%d*%d=%d\t", 4,1,4*1);
+    printf("%d*%d=%d\t", 4,2,4*2);
+    printf("%d*%d=%d\t", 4,3,4*3);
+    printf("%d*%d=%d\t", 4,4,4*4);
+    printf("%d*%d=%d\t", 4,5,4*5);
+    printf("%d*%d=%d\t", 4,6,4*6);
+    printf("%d*%d=%d\t", 4,7,4*7);
+    printf("%d*%d=%d\t", 4,8,4*8);
     printf("%d*%d=%d\n", 4,9,4*9);
-    printf("---------\n");
     
+    printf("%d*%d=%d\t", 5,1,5*1);
+    printf("%d*%d=%d\t", 5,2,5*2);
+    printf("%d*%d=%d\t", 5,3,5*3);
+    printf("%d*%d=%d\t", 5,4,5*4);
+    printf("%d*%d=%d\t", 5,5,5*5);
+    printf("%d*%d=%d\t", 5,6,5*6);
+    printf("%d*%d=%d\t", 5,7,5*7);
+    printf("%d*%d=%d\t", 5,8,5*8);
+    printf("%d*%d=%d\n", 5,9,5*9);
+
+    printf("%d*%d=%d\t", 6,1,6*1);
+    printf("%d*%d=%d\t", 6,2,6*2);
+    printf("%d*%d=%d\t", 6,3,6*3);
+    printf("%d*%d=%d\t", 6,4,6*4);
+    printf("%d*%d=%d\t", 6,5,6*5);
+    printf("%d*%d=%d\t", 6,6,6*6);
+    printf("%d*%d=%d\t", 6,7,6*7);
+    printf("%d*%d=%d\t", 6,8,6*8);
+    printf("%d*%d=%d\n", 6,9,6*9);
+
+    printf("%d*%d=%d\t", 7,1,7*1);
+    printf("%d*%d=%d\t", 7,2,7*2);
+    printf("%d*%d=%d\t", 7,3,7*3);
+    printf("%d*%d=%d\t", 7,4,7*4);
+    printf("%d*%d=%d\t", 7,5,7*5);
+    printf("%d*%d=%d\t", 7,6,7*6);
+    printf("%d*%d=%d\t", 7,7,7*7);
+    printf("%d*%d=%d\t", 7,8,7*8);
+    printf("%d*%d=%d\n", 7,9,7*9);
+
+    printf("%d*%d=%d\t", 8,1,8*1);
+    printf("%d*%d=%d\t", 8,2,8*2);
+    printf("%d*%d=%d\t", 8,3,8*3);
+    printf("%d*%d=%d\t", 8,4,8*4);
+    printf("%d*%d=%d\t", 8,5,8*5);
+    printf("%d*%d=%d\t", 8,6,8*6);
+    printf("%d*%d=%d\t", 8,7,8*7);
+    printf("%d*%d=%d\t", 8,8,8*8);
+    printf("%d*%d=%d\n", 8,9,8*9);
+
+    printf("%d*%d=%d\t", 9,1,9*1);
+    printf("%d*%d=%d\t", 9,2,9*2);
+    printf("%d*%d=%d\t", 9,3,9*3);
+    printf("%d*%d=%d\t", 9,4,9*4);
+    printf("%d*%d=%d\t", 9,5,9*5);
+    printf("%d*%d=%d\t", 9,6,9*6);
+    printf("%d*%d=%d\t", 9,7,9*7);
+    printf("%d*%d=%d\t", 9,8,9*8);
+    printf("%d*%d=%d\n", 9,9,9*9);
+
     return 0;
 }
